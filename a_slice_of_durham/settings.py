2168825cb55f9fe23c1fb1e5adf7a6e23cb3c013
ssciere <steve.sciere@gmail.com>
2019-06-05 20:18:55 -0400

    'more'

--- a_slice_of_durham/settings.py
+++ a_slice_of_durham/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0qkc+!&5gcv(%zx034rtfla2%@lekk$^@$5^1sa*ovq27_*^rq'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
